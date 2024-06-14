b73acad95fc1b7af2937da5367363c89bde92f72
Ivan Kishko1~ <oknavi779@gmail.com>
2024-06-14 20:11:11 +0200

    HW

---
+++ index.js
@@ -0,0 +1,27 @@
+// todo TASK 1
+const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+let str = "";
+for (let i = 0; i < friends.length; i++) {
+  str += friends[i] + ",";
+}
+console.log(str);
+
+const friends2 = ["Mango", "Poly", "Kiwi", "Ajax"];
+let str1 = "";
+str1 = friends2.join();
+console.log(str1);
+// todo TASK 2
+const cards = [
+  "Карточка-1",
+  "Карточка-2",
+  "Карточка-3",
+  "Карточка-4",
+  "Карточка-5",
+];
+
+const cardToRemove = cards.splice(2, 1);
+const cardToAdd = "Карточка-6";
+const cardToupdate = "Карточка-4";
+cards.push(cardToAdd);
+cards.splice(3, 1, cardToupdate);
+console.log(cards);
